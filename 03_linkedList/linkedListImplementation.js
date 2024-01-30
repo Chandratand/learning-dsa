c769be816987934de04872866416b10013bd2870
chandratand <Chandra.tand@gmail.com>
2024-01-30 22:00:12 +0700

    create simple singly linkedList

--- 03_linkedList/linkedListImplementation.js
+++ 03_linkedList/linkedListImplementation.js
@@ -10,18 +10,92 @@
 //     },
 //   },
 // };
-
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
 class LinkedList {
   constructor(value) {
     this.head = {
-      value: value,
+      value,
       next: null,
     };
     this.tail = this.head;
     this.length = 1;
   }
+
+  append(value) {
+    const newNode = new Node(value);
+    this.tail.next = newNode;
+    this.tail = newNode;
+    this.length++;
+  }
+  prepend(value) {
+    const newNode = new Node(value);
+    newNode.next = this.head;
+    this.head = newNode;
+
+    this.length++;
+  }
+
+  printList() {
+    const array = [];
+    let currentNode = this.head;
+    while (currentNode !== null) {
+      array.push(currentNode.value);
+      currentNode = currentNode.next;
+    }
+    return array;
+  }
+
+  insert(index, value) {
+    if (index >= this.length) {
+      return this.append(value);
+    }
+    if (index === 0) {
+      this.prepend(value);
+      return this.printList();
+    }
+
+    const newNode = new Node(value);
+    const leader = this.treverseToIndex(index - 1);
+    const holdingPrinter = leader.next;
+    leader.next = newNode;
+    newNode.next = holdingPrinter;
+    this.length++;
+    return this.printList();
+  }
+
+  treverseToIndex(index) {
+    // we can check the index is valid or not
+
+    let counter = 0;
+    let currentNode = this.head;
+    while (counter !== index) {
+      currentNode = currentNode.next;
+      counter++;
+    }
+    return currentNode;
+  }
+
+  remove(index) {
+    // we can check the index is valid or not
+    const leader = this.treverseToIndex(index - 1);
+    const unWantedNode = leader.next;
+    leader.next = unWantedNode.next;
+    this.length--;
+    return this.printList();
+  }
 }
 
 const myLinkedList = new LinkedList(10);
-
-console.log(myLinkedList);
+myLinkedList.append(5);
+myLinkedList.append(16);
+myLinkedList.prepend(1);
+myLinkedList.insert(2, 90);
+myLinkedList.insert(20, 88);
+myLinkedList.remove(2);
+const list = myLinkedList.printList();
+console.log(list);
